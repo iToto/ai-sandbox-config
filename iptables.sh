3c807acde554931d8c53fc02a778c49e4dd565bc
Salvatore D'Agostino <toto19@gmail.com>
2026-06-27 00:07:39 +0300

    update iptables

--- iptables.sh
+++ iptables.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# Run this once on the Ubuntu host after `docker compose up`
-# Adjust LAN subnet to match yours (check with `ip route`)
-
 LAN_SUBNET="192.168.1.0/24"
 CONTAINER_SUBNET="172.18.0.0/16"
 
 sudo iptables -I DOCKER-USER -s $CONTAINER_SUBNET -d $LAN_SUBNET -j DROP
 sudo iptables -I DOCKER-USER -s $CONTAINER_SUBNET -d 10.0.0.0/8 -j DROP
 sudo iptables -I DOCKER-USER -s $CONTAINER_SUBNET -d 172.16.0.0/12 -j DROP
+sudo iptables -I DOCKER-USER -s $CONTAINER_SUBNET -d 100.64.0.0/10 -j DROP
 
 sudo apt install -y iptables-persistent
 sudo netfilter-persistent save
